b642bf06fd69da614e068983d5c05d38923bc357
alfreddai <ya95123@gmail.com>
2024-08-15 18:18:57 +0800

    feat: open effect & fade-up effect

--- js/index.js
+++ js/index.js
@@ -3,17 +3,23 @@ const bannerBtn = document.getElementById("banner-btn");
 const openBtn = document.getElementById("open-btn");
 const openContent = document.querySelector(".open-content");
 
-const scroll = new LocomotiveScroll({
-  el: document.querySelector("[data-scroll-container]"),
-  smooth: true,
-});
+// TODO perf 一進頁面的 #hash scroll bug
+window.addEventListener("load", function () {
+  const scroll = new LocomotiveScroll({
+    el: document.querySelector("[data-scroll-container]"),
+    smooth: true,
+    reloadOnContextChange: true,
+  });
 
-bannerBtn.addEventListener("click", () => {
-  scroll.scrollTo(img1);
-});
+  bannerBtn.addEventListener("click", () => {
+    scroll.scrollTo(img1);
+  });
 
-openBtn.addEventListener("click", () => {
-  openContent.classList.remove("hide");
-  scroll.update();
-  scroll.scrollTo(openContent);
+  openBtn.addEventListener("click", () => {
+    if (!openContent.classList.contains("hide")) return;
+
+    openContent.classList.remove("hide");
+    scroll.update();
+    scroll.scrollTo(openContent);
+  });
 });
