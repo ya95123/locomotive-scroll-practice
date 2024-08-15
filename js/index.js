832b3346d1ae3a6475f8b2d214ff6cb6c07151c1
alfreddai <ya95123@gmail.com>
2024-08-15 12:00:44 +0800

    feat: basic layout & use basic scroll smooth & scrollto function

--- js/index.js
+++ js/index.js
@@ -1,4 +1,19 @@
+const img1 = document.getElementById("img-1");
+const bannerBtn = document.getElementById("banner-btn");
+const openBtn = document.getElementById("open-btn");
+const openContent = document.querySelector(".open-content");
+
 const scroll = new LocomotiveScroll({
   el: document.querySelector("[data-scroll-container]"),
   smooth: true,
+});
+
+bannerBtn.addEventListener("click", () => {
+  scroll.scrollTo(img1);
+});
+
+openBtn.addEventListener("click", () => {
+  openContent.classList.remove("hide");
+  scroll.update();
+  scroll.scrollTo(openContent);
 });
