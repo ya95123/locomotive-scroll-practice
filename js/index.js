3f4c95d416da006ae4757c37f3f13c6b9e5d02f1
alfreddai <ya95123@gmail.com>
2024-08-16 16:32:07 +0800

    feat: add progress for img1-title

--- js/index.js
+++ js/index.js
@@ -3,23 +3,27 @@ const bannerBtn = document.getElementById("banner-btn");
 const openBtn = document.getElementById("open-btn");
 const openContent = document.querySelector(".open-content");
 
-// TODO perf 一進頁面的 #hash scroll bug
-window.addEventListener("load", function () {
-  const scroll = new LocomotiveScroll({
-    el: document.querySelector("[data-scroll-container]"),
-    smooth: true,
-    reloadOnContextChange: true,
-  });
+// TODO (待解決) perf 一進頁面的 #hash scroll bug
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("[data-scroll-container]"),
+  smooth: true,
+});
 
-  bannerBtn.addEventListener("click", () => {
-    scroll.scrollTo(img1);
-  });
+bannerBtn.addEventListener("click", () => {
+  scroll.scrollTo(img1);
+});
 
-  openBtn.addEventListener("click", () => {
-    if (!openContent.classList.contains("hide")) return;
+openBtn.addEventListener("click", () => {
+  if (!openContent.classList.contains("hide")) return;
 
-    openContent.classList.remove("hide");
-    scroll.update();
-    scroll.scrollTo(openContent);
-  });
+  openContent.classList.remove("hide");
+  scroll.update();
+  scroll.scrollTo(openContent);
+});
+
+scroll.on("scroll", (args) => {
+  if (typeof args.currentElements["img1-title"] === "object") {
+    let progress = args.currentElements["img1-title"].progress;
+    console.log(progress);
+  }
 });
